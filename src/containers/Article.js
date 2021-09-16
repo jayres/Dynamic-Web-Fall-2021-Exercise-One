f9677dfd954427dc3f25c2f7cc564109a834ba3b
James Ayres <jfrancis.ayres@gmail.com>
2021-09-16 09:35:55 -0400

    Article Card and integrate data

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Article({ children }) {
+function Article() {
   const articleTitle = `Single Article`;
   return (
     <div>
--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,10 +1,15 @@
 import React from "react";
+import ArticleCard from "../components/ArticleCard";
+import Data from "../components/data.json";
 
 function ArticleListing() {
   const articlesTitle = `Articles`;
   return (
     <div className="PageWrapper">
       <h2 className="HeaderOneStyle">{articlesTitle}</h2>
+      {Data.map((article, key) => (
+        <ArticleCard article={article} key={key} />
+      ))}
     </div>
   );
 }
